099c5f19eafccd940be9d4ebad31b43a6b83ef1b
MarenWoodruff <wren403@gmail.com>
2014-03-17 10:26:37 -0700

    finished 10 controller tests

--- spec/controllers/coffeeshops_controller_spec.rb
+++ spec/controllers/coffeeshops_controller_spec.rb
@@ -46,6 +46,12 @@ describe CoffeeshopsController do
         get :edit, id: @coffeeshop
         expect(assigns(:coffeeshop)).to eq @coffeeshop
     end
+
+    it 'renders the :edit template' do
+        @coffeeshop = FactoryGirl.create(:coffeeshop) 
+        get :edit, id: @coffeeshop
+        expect(response).to render_template :edit
+    end
   end
 
 
@@ -57,51 +63,21 @@ describe CoffeeshopsController do
         put :update, {:id => @coffeeshop, :coffeeshop => { "name" => "Intelligentsia" }}
     end
 
-      # it "assigns the requested coffeeshop as @coffeeshop" do
-      #   coffeeshop = Coffeeshop.create! valid_attributes
-      #   put :update, {:id => coffeeshop.to_param, :coffeeshop => valid_attributes}, valid_session
-      #   assigns(:coffeeshop).should eq(coffeeshop)
-      # end
-
-      # it "redirects to the coffeeshop" do
-      #   coffeeshop = Coffeeshop.create! valid_attributes
-      #   put :update, {:id => coffeeshop.to_param, :coffeeshop => valid_attributes}, valid_session
-      #   response.should redirect_to(coffeeshop)
-      # end
-    # end
-
-    # describe "with invalid params" do
-    #   it "assigns the coffeeshop as @coffeeshop" do
-    #     coffeeshop = Coffeeshop.create! valid_attributes
-    #     # Trigger the behavior that occurs when invalid params are submitted
-    #     Coffeeshop.any_instance.stub(:save).and_return(false)
-    #     put :update, {:id => coffeeshop.to_param, :coffeeshop => { "name" => "invalid value" }}, valid_session
-    #     assigns(:coffeeshop).should eq(coffeeshop)
-    #   end
-
-    #   it "re-renders the 'edit' template" do
-    #     coffeeshop = Coffeeshop.create! valid_attributes
-    #     # Trigger the behavior that occurs when invalid params are submitted
-    #     Coffeeshop.any_instance.stub(:save).and_return(false)
-    #     put :update, {:id => coffeeshop.to_param, :coffeeshop => { "name" => "invalid value" }}, valid_session
-    #     response.should render_template("edit")
-    #   end
-    # end
   end
 
-  # describe "DELETE destroy" do
-  #   it "destroys the requested coffeeshop" do
-  #     coffeeshop = Coffeeshop.create! valid_attributes
-  #     expect {
-  #       delete :destroy, {:id => coffeeshop.to_param}, valid_session
-  #     }.to change(Coffeeshop, :count).by(-1)
-  #   end
+  describe "DELETE destroy" do
+    it "destroys the requested coffeeshop" do
+        @coffeeshop = FactoryGirl.create(:coffeeshop)
+        expect {
+            delete :destroy, id: @coffeeshop
+        }.to change(Coffeeshop, :count).by(-1)
+    end
 
-  #   it "redirects to the coffeeshops list" do
-  #     coffeeshop = Coffeeshop.create! valid_attributes
-  #     delete :destroy, {:id => coffeeshop.to_param}, valid_session
-  #     response.should redirect_to(coffeeshops_url)
-  #   end
-  # end
+    it "redirects to the coffeeshops list" do
+        @coffeeshop = FactoryGirl.create(:coffeeshop)
+        delete :destroy, id: @coffeeshop
+        response.should redirect_to(coffeeshops_url)
+    end
+  end
 
 end

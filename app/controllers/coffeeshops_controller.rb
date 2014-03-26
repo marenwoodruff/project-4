1212987ab2e38731397d4b7566f574829d825f25
MarenWoodruff <wren403@gmail.com>
2014-03-26 12:59:23 -0700

    fixed geolocation, add list of coffeeshops to index & autolocate from welcome

--- app/controllers/coffeeshops_controller.rb
+++ app/controllers/coffeeshops_controller.rb
@@ -3,12 +3,18 @@ class CoffeeshopsController < ApplicationController
 
   def index
     # return a Geocoder::Result which corresponds to your location
-    if params[:search].present? && Coffeeshop.present?
-      @coffeeshops = Coffeeshop.near(params[:search], 5).reverse
-    elsif params[:search_lat] && params[:search_lng]
-       @coffeeshops = Coffeeshop.near([params[:search_lat], params[:search_lng]], params[:distance] || 5)
-    else
-      @coffeeshops = Coffeeshop.all
+    if params[:search].present? 
+      respond_to do |format|
+        format.html {@coffeeshops = Coffeeshop.near(params[:search], 5).reverse}
+        format.json {@coffeeshops = Coffeeshop.near(params[:search], 5)}
+      end
+    else params[:search_lat] && params[:search_lng]
+      respond_to do |format|
+        format.html { @coffeeshops = Coffeeshop.near([params[:search_lat], params[:search_lng]], params[:distance] || 5).reverse }
+        format.json { @coffeeshops = Coffeeshop.near([params[:search_lat], params[:search_lng]], params[:distance] || 5) }
+      end 
+    # else
+    #   @coffeeshops = Coffeeshop.all
     end
   end
 
